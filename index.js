894069bcbe78fd0fd14e4e60ee54cdc5044248e3
Theo Pack <tf.pack@gmail.com>
2019-03-02 17:47:02 +0100

    Clean up

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-exports.handler = (data, context) => {
-  const pubSubMessage = data;
-  const name = pubSubMessage.data
-    ? Buffer.from(pubSubMessage.data, 'base64').toString()
-    : 'World';
+var googleAuth = require('google-auto-auth');
+var auth = googleAuth();
+var axios = require('axios');
 
-  console.log(`Hello, ${name}!`);
+function getAccessToken() {
+  return new Promise(resolve => {
+    auth.getToken((err, token) => {
+      resolve(token);
+    });
+  });
+}
+
+exports.handler = async _ => {
+  const token = await getAccessToken();
+
+  const client = axios.create({
+    baseURL: `https://gcr.io/v2`,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  const response = await client.get('/furi-kuri/cloudshell/tags/list');
+
+  for (var image in response.data.manifest) {
+    if (response.data.manifest.hasOwnProperty(image)) {
+      if (!response.data.manifest[image].tag.includes("latest")) {
+        console.log(`Delete ${image}`);
+        await client.delete('/furi-kuri/cloudshell/manifests/' + image);
+      } else {
+        console.log("Do not delete latest");
+      }
+    }
+  }
+
+  console.log("Finish")
 };
