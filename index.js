0fbbdf7998409f2808134e53dc0dada541955296
Theo Pack <tf.pack@gmail.com>
2019-03-02 16:13:00 +0100

    Add simpe pubsub example

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.handler = (data, context) => {
+  const pubSubMessage = data;
+  const name = pubSubMessage.data
+    ? Buffer.from(pubSubMessage.data, 'base64').toString()
+    : 'World';
+
+  console.log(`Hello, ${name}!`);
+};
